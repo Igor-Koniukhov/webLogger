a289c52cb2b0b9bf0aec65fc7811171049c8ac91
Igor Koniukhov <ikoniukhov@gmail.com>
2021-08-29 17:04:57 +0300

    added ability  customization logger functions

--- webLogger.go
+++ webLogger.go
@@ -18,41 +18,51 @@ var (
 	Reset       = "\033[0m"
 	brightWhite = "\033[30m"
 	Red         = "\033[31m"
+	RedB        = "\x1b[31;1m"
 	Green       = "\033[32m"
 	Yellow      = "\033[33m"
+	YellowB     = "\x1b[33;1m"
 	Blue        = "\033[34m"
+	BlueB       = "\x1b[34;1m"
 	Purple      = "\033[35m"
+	PurpleB     = "\x1b[35;1m"
 	Cyan        = "\033[36m"
 	White       = "\033[37m"
 )
 
 const (
-	Info           = "INFO"
-	Warning        = "WARNING"
-	Error          = "ERROR"
-	FatalL         = "FATAL"
+	Inf            = "INFO"
+	Wrn            = "WARNING"
+	Err            = "ERROR"
+	Ftl            = "FATAL"
+	Dbg            = "DEBUG"
 	Console        = 2
 	File           = 1
 	ConsoleAndFile = 3
 )
 
-var Log *LogStruct
-var mutual sync.Mutex
-
 type LogStructInterface interface {
 	timeGenerator(timeFormat string) string
 	getLocationPointer() (result string)
-	defaultData() (filePath, logFile, timeFormat string,  outWriter int)
+	defaultData() (filePath, logFile, timeFormat string, outWriter int)
 	infoWriter(alarmType, filePath string, n int, message string, params interface{})
-	Says(alarm string, msg string, err error)
 	ClientError(w http.ResponseWriter, status int)
 	ServerError(w http.ResponseWriter, err error)
+	Debug(err error)
+	Error(message ...interface{})
+	Info(message ...interface{})
+	Warning(message ...interface{})
+	Fatal(message ...interface{})
+
 }
+
+var Log *LogStruct
+var mutual sync.Mutex
 
 type LogParameters struct {
 	FilePath   string
 	LogFile    string
-	OutWriter        int
+	OutWriter  int
 	Params     interface{}
 	TimeFormat string
 }
@@ -83,15 +93,15 @@ func (l *LogStruct) getLocationPointer() (result string) {
 	result = fmt.Sprintf(" %s:%d ", fileName, line)
 	return
 }
-func(l LogStruct) defaultData() (filePath, logFile, timeFormat string,  outWriter int){
-	switch l.LogParameters.FilePath{
-	case  "":
+func (l LogStruct) defaultData() (filePath, logFile, timeFormat string, outWriter int) {
+	switch l.LogParameters.FilePath {
+	case "":
 		filePath = "./logger"
 		l.LogParameters.FilePath = filePath
 	default:
 		filePath = l.LogParameters.FilePath
 	}
-	switch l.LogParameters.LogFile{
+	switch l.LogParameters.LogFile {
 	case "":
 		logFile = "/logger.log"
 		l.LogParameters.LogFile = logFile
@@ -102,10 +112,10 @@ func(l LogStruct) defaultData() (filePath, logFile, timeFormat string,  outWrite
 	case "":
 		timeFormat = "2006/01/02 15:04:05"
 		l.LogParameters.TimeFormat = timeFormat
-	default :
+	default:
 		timeFormat = l.LogParameters.TimeFormat
 	}
-	switch l.LogParameters.OutWriter{
+	switch l.LogParameters.OutWriter {
 	case 0:
 		outWriter = Console
 		l.LogParameters.OutWriter = outWriter
@@ -115,7 +125,7 @@ func(l LogStruct) defaultData() (filePath, logFile, timeFormat string,  outWrite
 	return
 }
 
-func (l LogStruct) infoWriter(alarmType, message string, param interface{})  {
+func (l LogStruct) infoWriter(alarmType string, param interface{}) {
 	mutual.Lock()
 	defer mutual.Unlock()
 
@@ -131,10 +141,11 @@ func (l LogStruct) infoWriter(alarmType, message string, param interface{})  {
 	checkLogFileError(err)
 
 	alarmMap := map[string]string{
-		"info":    "\x1b[34;1m",
-		"warning": "\x1b[33;1m",
-		"error":   "\x1b[31;1m",
-		"fatal":   "\033[31m",
+		"info":    BlueB,
+		"warning": YellowB,
+		"error":   RedB,
+		"fatal":   RedB,
+		"debug":   PurpleB,
 	}
 	alT := strings.ToLower(alarmType)
 	alarmColor := alarmMap[alT]
@@ -143,7 +154,7 @@ func (l LogStruct) infoWriter(alarmType, message string, param interface{})  {
 		fl, err := os.OpenFile(filePath+logFile, os.O_CREATE|os.O_APPEND|os.O_RDWR, 0644)
 		defer fl.Close()
 		checkLogFileError(err)
-		fileStmt := fmt.Sprintf(" %s %s %v %s %s \n", alarmType, timePointer, param, location, message)
+		fileStmt := fmt.Sprintf(" %s %s %v %s \n", alarmType, timePointer, param, location)
 
 		io.Copy(fl, strings.NewReader(fileStmt))
 
@@ -156,7 +167,7 @@ func (l LogStruct) infoWriter(alarmType, message string, param interface{})  {
 			fmt.Print(brightWhite, "W:")
 		}
 
-		consoleStmt := fmt.Sprintf("|%s|%s %v%s %s%s %s%s \n", alarmType, Reset, param, Blue, timePointer, location, Reset, message)
+		consoleStmt := fmt.Sprintf("|%s|%s %v%s %s%s %s \n", alarmType, Reset, param, Blue, timePointer, location, Reset)
 
 		io.Copy(os.Stdout, strings.NewReader(alarmColor+consoleStmt+Reset))
 
@@ -164,21 +175,37 @@ func (l LogStruct) infoWriter(alarmType, message string, param interface{})  {
 
 }
 
-func (l LogStruct) Says(alarmType string, msg string, err error) {
-	l.infoWriter(alarmType, msg, err)
-}
-
 func (l LogStruct) ClientError(w http.ResponseWriter, status int) {
 	msg := fmt.Sprintf("Client error with status of %v ", status)
-	l.infoWriter(Error,  msg, status)
+	l.infoWriter(Err, msg)
 	http.Error(w, http.StatusText(status), status)
 }
 
 func (l LogStruct) ServerError(w http.ResponseWriter, err error) {
 	trace := fmt.Sprintf("%s\n%s", err.Error(), debug.Stack())
-	l.infoWriter(Error,  trace, err)
+	l.infoWriter(Err, trace)
 	http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
 }
+
+func (l LogStruct) Debug(err error) {
+	trace := fmt.Sprintf("%s\n%s", err.Error(), debug.Stack())
+	l.infoWriter(Dbg, fmt.Sprint(trace))
+}
+
+func (l LogStruct) Error(message ...interface{}) {
+	l.infoWriter(Err, fmt.Sprint(message...))
+}
+func (l LogStruct) Info(message ...interface{}) {
+	l.infoWriter(Inf, fmt.Sprint(message...))
+}
+func (l LogStruct) Warning(message ...interface{}) {
+	l.infoWriter(Wrn, fmt.Sprint(message...))
+}
+func (l LogStruct) Fatal(message ...interface{}) {
+	l.infoWriter(Ftl, fmt.Sprint(message...))
+}
+
+
 
 func checkLogFileError(err error) {
 	if err != nil {
@@ -186,5 +213,3 @@ func checkLogFileError(err error) {
 		lp.Println(Red, "Could'n open file", err)
 	}
 }
-
-
